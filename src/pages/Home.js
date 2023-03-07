7ed494309333a19bce7020d49d0b9b3386706456
Firat Uctepe <firat.uctepe1@gmail.com>
2023-03-07 17:13:11 +0300

    ...

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -18,7 +18,7 @@ export default function Home() {
 
     useEffect(() => {
         loadUser();
-    }, [users])
+    }, [])
 
     return (
         <div>
@@ -43,14 +43,15 @@ export default function Home() {
                                         <th>{user.userName}</th>
                                         <th>{user.email}</th>
                                         <th>
-                                            <button className='btn btn-primary mx-2'>View</button>
+                                            <Link className='btn btn-primary mx-2'
+                                            to={`/viewuser/${user.id}`}
+                                            >View</Link>
                                             <Link className='btn btn-outline-primary mx-2'
                                             to={`/edituser/${user.id}`}
                                             >Edit</Link>
                                             <button className='btn btn-danger mx-2'
                                             onClick={()=>deleteUser(user.id)}
-                                            >Delete
-                                            </button>
+                                            >Delete</button>
                                         </th>
                                     </tr>
                                 )
